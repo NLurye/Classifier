0746f0fa6cff9dd03e1bf6e3a0d67189b1d84245
NLurye <anastasia.lurye@gmail.com>
2023-06-09 21:16:04 +0300

    entropy1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 class Prediction:
 
     def __init__(self, train_data):
-        self.ttl_examples, self.train_examples = self.parse_data(train_data)
+        self.feature_to_predict, self.ttl_examples, self.train_examples = self.parse_data(train_data)
         self.count_dict = self.count_features(self.train_examples)
 
     def parse_data(self, txt):
@@ -16,7 +16,7 @@ class Prediction:
         for line in lines[1:]:
             values = line.replace("\n", "").split('\t')
             data.append(dict(zip(header, values)))
-        return len(data), data
+        return header[-1], len(data), data
 
     def get_entropy(self, pos_examples, neg_examples):
         p_neg = neg_examples / self.ttl_examples
@@ -29,15 +29,16 @@ class Prediction:
     def get_gain(self):
         pass
 
-    def count_features(self, examples):
+    def count_features(self, examples, feature_to_predict):
         feature_options = {}
         for example in examples:
             for feature, option in example.items():
                 if feature not in feature_options:
                     feature_options[feature] = {}
                 if option not in feature_options[feature]:
-                    feature_options[feature][option] = 0
-                feature_options[feature][option] += 1
+                    feature_options[feature][option] = (0, 0)
+                if example[feature_to_predict] == None:
+                    feature_options[feature][option] += 1
         return feature_options
 
 
