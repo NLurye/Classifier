51258a0bed6ee4adcfb893c881883cfab6bbdf30
NLurye <anastasia.lurye@gmail.com>
2023-06-13 22:21:46 +0300

    adam

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import numpy as np
+np.random.seed(0)
+
+class NaiveBayes:
+    def __init__(self):
+        pass
+
+    def predict(self, example):
+        pass
 
 
 class Tree:
@@ -21,35 +29,47 @@ class Tree:
         child_subtree = self.children[feature_value]
         return child_subtree.predict(example)
 
-    def print_tree(self, indent=''):
+    def print_tree(self, file, indent=''):
         if self.branch is not None:
             print(indent + self.branch)
             return
 
         for branch, subtree in self.children.items():
             print(indent + self.feature + ' = ' + str(branch) + ':')
-            subtree.print_tree(indent + '\t' + '\t\t')
+            subtree.print_tree(file, indent + '\t' + '\t\t')
 
 
 class Classifier:
 
     def __init__(self, train_data):
-        self.features = None
         self.info_gains = None
         self.all_entropies = None
         self.feature_to_predict, self.ttl_examples, self.train_examples = self.parse_data(train_data)
         self.count_dict = self.count_features(self.train_examples)
         self.tree = Tree()
+        self.bayes_model = NaiveBayes()
 
-    def parse_data(self, txt):
+    def parse_data(self, txt, test=False):
         with open(txt, 'r') as file:
             lines = file.readlines()
         data = []
         header = lines[0].replace("\n", "").split('\t')
         for line in lines[1:]:
             values = line.replace("\n", "").split('\t')
+            if test:
+                values.pop()
             data.append(dict(zip(header, values)))
         return header[-1], len(data), data
+
+    def set_test_data(self, test_file):
+        feature_to_predict, ttl_examples, self.test_dict = self.parse_data(test_file, test=True)
+
+    def predict_on_test_data(self, file):
+        with open(file, 'w') as f:
+            for example in self.test_dict:
+                tree_prediction = self.tree.predict(example)
+                bayes_prediction = self.bayes_model.predict(example)
+                f.write(str(tree_prediction) + ' ' + str(bayes_prediction) + '\n')
 
     def get_entropy(self, neg_examples, pos_examples):
         ttl_examples = neg_examples + pos_examples
@@ -99,7 +119,6 @@ class Classifier:
         for feature, entropies in new_entropies.items():
             info_gains[feature] = self.get_gain_for_feature(entropies_for_feature=entropies)
         return info_gains
-    # TODO: check if we can assume yes or no in predicted feature
 
     def count_features(self, examples):
         feature_options = {}
@@ -120,27 +139,32 @@ class Classifier:
         return max(gains, key=gains.get)
 
     def mode(self, values):
-        max(set(values), key=values.count)
+        n_yes = values.count('Yes')
+        n_no = values.count('no')
+        if n_yes >= n_no:
+            return 'Yes'
+        else:
+            return 'no'
 
     def make_tree(self):
         features = list(self.count_dict.keys())
         self.all_entropies = self.calc_entropies_for_all(features, self.train_examples)
         self.info_gains = self.calc_gain_for_all(features, self.train_examples)
-        self.features = list(self.info_gains.keys())
-        self.tree = self.build_tree(self.train_examples, self.info_gains, None)
+        features = list(self.info_gains.keys())
+        self.tree, _ = self.build_tree(self.train_examples, self.info_gains, None, features)
 
-    def build_tree(self, examples, info_gains, default):
+    def build_tree(self, examples, info_gains, default, features):
         # Examples is empty return default
         if len(examples) == 0:
-            return default
+            return default, features
         # All examples have the same classification - return classification
         if len(set([example[self.feature_to_predict] for example in examples])) == 1:
-            return Tree(value=examples[0][self.feature_to_predict])
+            return Tree(value=examples[0][self.feature_to_predict]), features
         # No more features to split on
         if len(info_gains) == 1:
             predicted_feature_values = [example[self.feature_to_predict] for example in examples]
             most_common_value = self.mode(predicted_feature_values)
-            return Tree(value=most_common_value)
+            return Tree(value=most_common_value), features
 
         best_feature = self.get_most_informative(info_gains)
 
@@ -156,20 +180,23 @@ class Classifier:
                 most_common_value = self.mode(predicted_feature_values)
                 subtree = Tree(value=most_common_value)
             else:
-                if best_feature in self.features:
-                    self.features.remove(best_feature)
-                updated_gains = self.calc_gain_for_all(self.features, filtered_examples)
-                subtree = self.build_tree(filtered_examples, updated_gains, self.mode(predicted_feature_values))
+                if best_feature in features:
+                    features.remove(best_feature)
+                updated_gains = self.calc_gain_for_all(features, filtered_examples)
+                default = self.mode(predicted_feature_values)
+                subtree, features = self.build_tree(filtered_examples, updated_gains, default, features)
             tree.add_child(option, subtree)
 
-        return tree
+        return tree, features
 
-# TODO: Temp. gain 0.06423863677845937
 
 if __name__ == '__main__':
-    file_path = 'test2.txt'
-    p = Classifier(file_path)
+    train_file = 'test2.txt'
+    p = Classifier(train_file)
+    test_file = 'test2.txt'
+    p.set_test_data(test_file)
     p.make_tree()
-    p.tree.print_tree()
+    p.tree.print_tree(file='my_output_tree.txt')
+    # p.predict_on_test_data(file='my_output.txt')
     pass
 
